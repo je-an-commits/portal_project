f4a2cad6d6e172c43dcb3e8b4f027f1cc1e5e21b
Jerico Andrei Abalos <ja.abalos18@gmail.com>
2026-06-05 16:44:03 +0800

    axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:3000",
-=======
   baseURL: "https://portal-project-kb2y.onrender.com",
->>>>>>> 0392bb9f6359c80bea6b680571003904ab0dda09
 });
 
 export default api;
